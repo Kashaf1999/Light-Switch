02dfcc25a4a6e7688a958d7eecdeea8dedeb9e14
Kashaf <kkashaf273@gmail.com>
2022-08-06 13:37:52 +0500

    First Commit

---
+++ index.js
@@ -0,0 +1,63 @@
+// console.time("Your Code Took");
+// console.log("Kashaf");
+// console.log(5*4);
+// console.log(9/3);
+// console.log(10%3);
+// console.log({Kashaf:55 , Khalid:60});
+// console.log({Kashaf: "Khalid"});
+// console.timeEnd("Your Code Took");
+// console.error("This is Warning");
+
+
+// let a = "Kashaf";
+// b = 99;
+//  console.log(a + b);
+// let a = "Kashaf"
+// document.write(a);
+//  function myFunction(a,b){
+//      document.write(`Hello ${a} and your father name is ${b} am i write?`)
+// //  };
+// //  myFunction('Kashaf','Khalid');
+// document.write("Total Subjects - 5 <br> <br>");
+// document.write("Total Marks - 500 <br> <br>");
+// function myFunction(Urdu, English, Math, Computer, Biology) {
+//     let marks = Urdu + English + Math + Computer + Biology;
+//     return marks;
+// };
+// let marksObtain = myFunction(60, 70, 80, 80, 90);
+// document.write("You Got " + marksObtain + " marks" + "<br> <br>");
+
+// function total(totalMarks){
+//     let percent = totalMarks*100/500;
+//     return percent;
+// }
+// let percentage = total(marksObtain);
+// document.write("You got "+ percentage +" %");
+//  function fun(){
+//      alert("Don't Touch Me")
+//  };
+
+// function myFunction(a,b){
+//   let sum = a*b;
+//   return sum;
+// };
+// let multiply = (5*3); 
+// document.getElementById('demo').innerHTML = multiply;
+
+// var x = myFunction(4, 3);
+// document.getElementById("demo").innerHTML = x;
+
+// function myFunction(a, b) {
+//   return a * b;
+// }
+// function newFunction(){
+//   document.getElementById('demo').innerHTML = Date();
+// }
+var image = document.getElementById('myImg');
+function bulbOn() {
+  image.src='https://www.w3schools.com/js/pic_bulbon.gif';
+}
+
+function bulbOff() {
+  image.src='https://www.w3schools.com/js/pic_bulboff.gif';
+}
